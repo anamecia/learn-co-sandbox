3098eec47ec7ca3ca7b9ed1d79219354deafc776
Ana Robalo <anameciarobalo@gmail.com>
2019-11-18 13:35:09 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,16 @@
+def greeting 
+  puts 'Hello World'
+end
+
+greeting
+greeting
+greeting
+greeting
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+end
+
+say_greeting_five_times
